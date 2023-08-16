e731dc01c687f0789b0d105830106deff2488acb
= <irismickens@gmail.com>
2023-08-16 15:21:07 -0500

    Update margiela.py

--- modules/margiela.py
+++ modules/margiela.py
@@ -157,11 +157,13 @@ class Margiela(Bot):
             page.add_button(
                 "prev",
                 emoji=Emoji.left,
+                label="Previous",
                 style=discord.ButtonStyle.blurple,
             )
             page.add_button(
                 "next",
                 emoji=Emoji.right,
+                label="Next",
                 style=discord.ButtonStyle.blurple,
             )
             page.add_button(
