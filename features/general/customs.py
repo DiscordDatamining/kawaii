35c8f3fb2179a093f1df918be595466042424d53
= <irismickens@gmail.com>
2023-08-16 18:27:02 -0500

    Create customs.py

---
+++ features/general/customs.py
@@ -0,0 +1,16 @@
+from discord import Embed
+from discord.ext.commands import (
+    Cog,
+    group,
+    command,
+)
+from modules.margiela import Margiela
+
+
+class AutoWorker(Cog):
+    def __init__(self: "AutoWorker", bot: Margiela, *args, **kwargs) -> None:
+        self.bot, self.client, self.worker = bot
+
+
+async def setup(bot):
+    await bot.add_cog(AutoWorker(bot))
